0825edae4f5962960d5ed64249a02e98aab11028
Aryan Jabbari <AryanJabbari@gmail.com>
2023-12-12 13:22:29 -0500

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import { GTagInitializer } from './homepage/GTagInitializer';
 
@@ -14,8 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <GTagInitializer />
         {children}
+        <GTagInitializer />
+        <Analytics />
       </body>
     </html>
   );
